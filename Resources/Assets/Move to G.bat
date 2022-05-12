840f5222b304b2a6c6ceee5b83f0be4d5eb4b9ed
Zane Stockton <47407590+A-Force-of-Will@users.noreply.github.com>
2022-05-12 14:41:00 -0600

    Delete Move to G.bat

--- Resources/Assets/Move to G.bat
+++
@@ -1,5 +0,0 @@
-REM Copying Net-Hooks To G:
-@Echo Off
-xcopy "C:\Users\zstockton\Documents\Nhooks\RoutechToFiveAxis\bin\x64\Debug\RoutechToFiveAxis.dll" "\\thomas\eng\CAD Support\CNC\(00) NET-HOOKS\Hooks\" /E /C /I /Q /H /R /K /Y
-xcopy "C:\Users\zstockton\Documents\Nhooks\RoutechToFiveAxis\Resources\FunctionTable\RoutechToFiveAxis.ft" "\\thomas\eng\CAD Support\CNC\(00) NET-HOOKS\Hooks\" /E /C /I /Q /H /R /K /Y
-PAUSE
